b961bb331bf88d285824c25d202f27dd2459bc70
AMOS KIPKORIR <amos.kipkorir@student.moringaschool.com>
2025-06-24 16:56:01 +0300

    adds latest commits

--- main.js
+++ main.js
@@ -1,20 +1,8 @@
-// Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
-
-// Your JavaScript code goes here!
-
-
-
-
-//------------------------------------------------------------------------------
-// Don't change the code below: this function mocks the server response
-//------------------------------------------------------------------------------
-
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
+// Provided function (you already have this)
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      let isRandomFailure = Math.random() < 0.2;
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
@@ -23,3 +11,40 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
+// Wait for the DOM to load
+document.addEventListener("DOMContentLoaded", () => {
+  const errorModal = document.getElementById("modal");
+  const hearts = document.querySelectorAll(".like-glyph");
+
+  // Initially hide the error modal
+  errorModal.classList.add("hidden");
+
+  hearts.forEach((heart) => {
+    heart.addEventListener("click", () => {
+      mimicServerCall()
+        .then(() => {
+          if (heart.innerText === "♡") {
+            heart.innerText = "♥";
+            heart.classList.add("activated-heart");
+          } else {
+            heart.innerText = "♡";
+            heart.classList.remove("activated-heart");
+          }
+        })
+        .catch((error) => {
+          // Show error modal with message
+          errorModal.classList.remove("hidden");
+          const modalMessage = document.getElementById("modal-message");
+          if (modalMessage) {
+            modalMessage.innerText = error;
+          }
+
+          // Hide modal after 3 seconds
+          setTimeout(() => {
+            errorModal.classList.add("hidden");
+          }, 3000);
+        });
+    });
+  });
+});
